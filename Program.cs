75c4af7d3d7e122078b70ccc9ad6937c4d43612b
Dogan Yaman <yamandogan963@gmail.com>
2024-01-12 23:08:56 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace algoritma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen aralarında virgül olacak şekilde bir adet string değer ve bir adet index numrası giriniz : ");
+            string ifade = Console.ReadLine();
+            Algoritma(ifade);
+            
+        }
+
+        static void Algoritma(string ifade)
+        {
+            string[] dizi = ifade.Split(',');
+            string yazi = dizi[0];
+            int sayi = int.Parse(dizi[1]);
+            if(sayi <= dizi[0].Length)
+                yazi = yazi.Remove(sayi,1);
+            Console.WriteLine(yazi);
+        }
+    }
+}
